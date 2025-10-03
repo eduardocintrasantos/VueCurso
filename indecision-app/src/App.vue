b0c4bcffec0e45e578c05c8ce15477dbfefbb961
Eduardo Cintra dos Santos <eduardocintradossantos@gmail.com>
2025-10-03 13:28:02 -0300

    03-10-2025

--- indecision-app/src/App.vue
+++ indecision-app/src/App.vue
@@ -1,17 +1,11 @@
 <template>
-  <h1 class="text-3xl font-bold text-red-500">Tailwind OK</h1>
+  <IndecisionView />
 
   <hr />
-
-  <MyCounter :value="1" />
-  <MyCounterScript :value="2" />
-  <MyCounterScript2 :value="3" />
 </template>
 
 <script lang="ts" setup>
-import MyCounterScript2 from './components/my-counter-script/MyCounterScript2.vue';
-import MyCounter from './components/MyCounter.vue';
-import MyCounterScript from './components/my-counter-script/MyCounterScript2.vue';
+import IndecisionView from './views/IndecisionView.vue';
 </script>
 
 <style scoped></style>
---
+++ indecision-app/src/views/IndecisionView.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import ChatMessages from '@/components/chat/ChatMessages.vue';
+
+import type { ChatMessage } from '@/interfaces/chat-message.interface';
+import { ref } from 'vue';
+
+const messages = ref<ChatMessage[]>([
+  {
+    id: new Date().getTime(),
+    message: 'Ola Mundo',
+    itsMine: true,
+  },
+  {
+    id: new Date().getTime(),
+    message: 'Teste',
+    itsMine: true,
+  },
+  {
+    id: new Date().getTime() + 1,
+    message: 'No',
+    itsMine: false,
+    image: 'https://yesno.wtf/assets/no/4-122be48db47678331dbef3dd6ac6ff56.gif',
+  },
+  {
+    id: new Date().getTime(),
+    message: 'Eduardo',
+    itsMine: true,
+  },
+  {
+    id: new Date().getTime() + 1,
+    message: 'No',
+    itsMine: false,
+    image: 'https://yesno.wtf/assets/no/4-122be48db47678331dbef3dd6ac6ff56.gif',
+  },
+]);
+</script>
+
+<template>
+  <div class="bg-gray-100 h-screen flex flex-col max-w-lg mx-auto">
+    <div class="bg-blue-500 p-4 text-white flex justify-between items-center">
+      <span>Mi esposa</span>
+    </div>
+
+    <!-- ChatMessages -->
+    <ChatMessages :messages="messages" />
+
+    <!-- MessageBox -->
+    <MessageBox />
+  </div>
+</template>
